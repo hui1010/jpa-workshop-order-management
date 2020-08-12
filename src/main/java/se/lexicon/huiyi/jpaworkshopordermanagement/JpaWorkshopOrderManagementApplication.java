e67256101d2bfe1bb93895a35dc74794f4144625
deltagare <huiyi.skarner@gmail.com>
2020-08-12 09:42:11 +0200

    initial

---
+++ src/main/java/se/lexicon/huiyi/jpaworkshopordermanagement/JpaWorkshopOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.huiyi.jpaworkshopordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWorkshopOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaWorkshopOrderManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/huiyi/jpaworkshopordermanagement/JpaWorkshopOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.huiyi.jpaworkshopordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWorkshopOrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
