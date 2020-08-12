2d7c926ecaca2cc138c2b9a33b3f6d97f4dc87dd
deltagare <huiyi.skarner@gmail.com>
2020-08-12 10:30:22 +0200

    created the Product class

---
+++ src/main/java/se/lexicon/huiyi/jpaworkshopordermanagement/entity/Product.java
@@ -0,0 +1,63 @@
+package se.lexicon.huiyi.jpaworkshopordermanagement.entity;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Product {
+    private int id;
+    private String name;
+    private BigDecimal price;
+
+    public Product() {
+    }
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id &&
+                Objects.equals(name, product.name) &&
+                Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
